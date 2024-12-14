abe77a2389a58b18752666af014e3b0005d0a0ca
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2024-12-14 23:46:15 +0300

    mkdocs updated

--- tests/test_game_app.py
+++ tests/test_game_app.py
@@ -37,6 +37,11 @@ def test_empty_search(client):
     assert response.status_code == 200
     assert "Start typing to search for movies" in response.text
 
+def test_no_guess_provided(client):
+    response = client.post("/guess", data={"query": ""})
+    assert response.status_code == 200
+    assert "Please select a movie to guess" in response.text
+
 # def test_wrong_guess(client):
 #     # First get a game started
 #     client.get("/")
@@ -46,8 +51,3 @@ def test_empty_search(client):
 #     assert response.status_code == 200
 #     assert "Wrong guess" in response.text
 #     assert "backdrop-container" in response.text  # Should show next backdrop
-
-def test_no_guess_provided(client):
-    response = client.post("/guess", data={"query": ""})
-    assert response.status_code == 200
-    assert "Please select a movie to guess" in response.text
